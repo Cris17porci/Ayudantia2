e1bf91dbc6afb2215f3a74761f9c2f2d59d063ee
Cris17porci <cristobal.perez@ceinf.cl>
2013-04-29 00:24:39 -0400

    Version con Regla en algoritmo

--- login.php
+++ login.php
@@ -1,26 +1,38 @@
 <html>
-<head>
-<title>Login</title>
-</head>
-<body>
-<?php
-$usuario="cisse";
-$contraseña="123";
-$user=$_POST['user'];
-$pass=$_POST['pass'];
-
-if ($user!=$usuario)
-{
-echo "Usuario incorrecto";
-}
-elseif ($pass!=$contraseña)
-{
-echo "Contraseña incorrecta";
-}
-else
-{
-echo "<a href=\"DIRECCION DEL ENLACE\">Texto de enlace</a>";
-}
-?>
-</body>
+    <head>
+        <title>Login ISW</title>
+    </head>
+    <body>
+        <?php
+        $usuario="isw";
+        $contraseña="isw";
+        $user=$_POST['user'];
+        $pass=$_POST['pass'];
+        
+        if ($user!=$usuario)
+            {
+            echo "Usuario incorrecto Vuelva atras para ingresar nuevamente  ";
+            ?>
+            <a href="<?=$_SERVER["HTTP_REFERER"]?>">  Regresar  </a>
+            <?php
+            
+            }
+            elseif ($pass!=$contraseña)
+                {
+                echo "Contraseña incorrecta Vuelva atras para ingresar nuevamente      ";
+                ?>
+            <a href="<?=$_SERVER["HTTP_REFERER"]?>">  Regresar  </a>
+                <?php
+                }
+                
+                else
+                    {
+                    ?>
+            <h1>A Ingresado Correctamente </h1>
+            <h2> Recuerde que este es solo un prueba de un login para ISW 2013/01</h2>
+                <?php
+                
+                    }
+                    ?>
+    </body>
 </html>
